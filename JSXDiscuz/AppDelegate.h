1473c07f8c00bd78bdfbf1a141526badc4f2a175
zhousiyang <124876144@qq.com>
2018-03-16 09:59:43 +0800

    Initial Commit

---
+++ JSXDiscuz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSXDiscuz
+//
+//  Created by 周思扬 on 2018/3/16.
+//  Copyright © 2018年 周思扬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSXDiscuz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSXDiscuz
+//
+//  Created by 周思扬 on 2018/3/16.
+//  Copyright © 2018年 周思扬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
